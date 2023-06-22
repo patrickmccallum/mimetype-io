f7829cb4181d719fabe02425d1727fcf764f0da4
Patrick McCallum <patrickmccallum49@gmail.com>
2023-06-23 06:02:28 +1000

    chore: analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         title: `Gatsby Default Starter`,
         description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
         author: `@gatsbyjs`,
-        siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`
+        siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
     },
     plugins: [
         `gatsby-plugin-image`,
@@ -20,33 +20,43 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
-                path: `${__dirname}/src/images`
-            }
+                path: `${__dirname}/src/images`,
+            },
         },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
             resolve: `gatsby-plugin-google-fonts`,
             options: {
-                fonts: [
-                    `Merriweather`,
-                    `Open Sans`
-                ]
-            }
+                fonts: [`Merriweather`, `Open Sans`],
+            },
         },
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `mimetype.io`,
+                short_name: `mimetype.io`,
                 start_url: `/`,
-                background_color: `#663399`,
+                background_color: `#FFF`,
                 // This will impact how browsers show your PWA/website
                 // https://css-tricks.com/meta-theme-color-and-trickery/
                 // theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-            }
-        }
-    ]
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "386423877",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+            },
+        },
+    ],
 }
