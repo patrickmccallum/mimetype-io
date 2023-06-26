c6ace50a3b14be7cfd06b941133a03ec6006f189
Patrick McCallum <patrickmccallum49@gmail.com>
2023-06-26 12:48:35 +1000

    feat: make button clearer

--- src/shared/Button/SecondaryButton.tsx
+++ src/shared/Button/SecondaryButton.tsx
@@ -18,7 +18,7 @@ export const SecondaryButton = ({
         <button
             onClick={onClick}
             className={classNames(
-                "hover:white flex items-center rounded-md border border-slate-200 bg-white text-slate-500 transition-all duration-100 hover:text-slate-600 hover:shadow",
+                "hover:white flex items-center rounded-md border border-slate-300 bg-slate-50 text-slate-800 shadow-sm transition-all duration-100 hover:bg-slate-100 hover:text-slate-600 hover:shadow",
                 {
                     "gap-2 px-4 py-2 text-sm": size === "lg",
                     "gap-2 px-3 py-1 text-sm": size === "md",
