99f4fa0d5991d264974b6b94f55d9008892a7b0a
Patrick McCallum <patrickmccallum49@gmail.com>
2023-06-26 20:16:30 +1000

    feat: analytics changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,16 +48,20 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-                // The property ID; the tracking code won't be generated without it
-                trackingId: "G-XPG13CGVHN",
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Delays sending pageview hits on route update (in milliseconds)
-                pageTransitionDelay: 0,
+                trackingIds: [
+                    "G-XPG13CGVHN", // Google Analytics / GA
+                ],
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Delays processing pageview events on route update (in milliseconds)
+                    delayOnRouteUpdate: 0,
+                },
             },
         },
     ],
